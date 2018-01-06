424a329254809100f7b47462d38aff2fbfb7316a
RJ <jensenrs@gmail.com>
2018-01-05 16:29:17 -0800

    setting up project structure

---
+++ db/migrations/20180105161407_player_star.js
@@ -0,0 +1,15 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('player_star', table => {
+    table.increments()
+    table.integer('player_id')
+    table.foreign('player_id').references('id').inTable('player')
+    table.integer('star_id')
+    table.foreign('star_id').references('id').inTable('star')
+    table.integer('score').notNullable().defaultTo(0)
+  })
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('player_star')
+};
