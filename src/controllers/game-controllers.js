bbe37498ee5a66f16a02328ca6ba44fe8c1b6c05
RJ <jensenrs@gmail.com>
2018-01-08 10:17:30 -0800

    getting games with stars

--- src/controllers/game-controllers.js
+++ src/controllers/game-controllers.js
@@ -2,23 +2,23 @@ const model = require('../models/game-models')
 
 class GameController {
 
-  static getAllGames(req, res, next){
-    model.getAllGames().then(games => {
+  // static getAllGames(req, res, next){
+  //   model.getAllGames().then(games => {
+  //     res.json(games)
+  //   })
+  // }
+
+  static getAllGamesWithStars(req, res, next){
+    model.getAllGamesWithStars().then(games => {
       res.json(games)
     })
   }
 
-  // static getAllGamesWithStars(req, res, next){
-  //   model.getAllGamesWithStars().then(games => {
-  //     res.json(games)
-  //   })
-  // }
-  //
-  // static starsByGameId(req, res, next){
-  //   model.starsByGameId(req.params.gameId).then(games => {
-  //     res.json(games)
-  //   })
-  // }
+  static starsByGameId(req, res, next){
+    model.starsByGameId(req.params.gameId).then(games => {
+      res.json(games)
+    })
+  }
 
 }
 
--- src/routes/game-routes.js
+++ src/routes/game-routes.js
@@ -2,7 +2,8 @@ const express = require('express')
 const router = express.Router()
 const gameController = require('../controllers/game-controllers')
 
-router.get('/', gameController.getAllGames)
+router.get('/', gameController.getAllGamesWithStars)
+
 
 
 module.exports = router
