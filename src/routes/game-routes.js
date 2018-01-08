a2b3d4486452f65c8b2bff487db7ee63ae3805ae
RJ <jensenrs@gmail.com>
2018-01-08 08:52:21 -0800

    removed redundant part of url for get route

--- src/routes/game-routes.js
+++ src/routes/game-routes.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const gameController = require('../controllers/game-controllers')
 
-router.get('/games', gameController.getAllGames)
+router.get('/', gameController.getAllGames)
 
 
 module.exports = router
