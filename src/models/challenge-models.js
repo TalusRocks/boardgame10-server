6757b3096197dc7444548dd7635e98b6b16572f2
RJ <jensenrs@gmail.com>
2018-01-09 16:00:04 -0800

    getOneChallenge model returns data with games and stars

--- src/models/challenge-models.js
+++ src/models/challenge-models.js
@@ -6,6 +6,31 @@ class ChallengeModel {
     return knex('challenge')
       .where({id})
       .first()
+      .then(challenge => {
+        const id = challenge.id
+        return knex('challenge_game')
+          .select('game.id', 'game.name')
+          .innerJoin('game', 'game_id', 'game.id')
+          .where('challenge_id', id)
+          .then(games => {
+            challenge.games = games
+            return challenge
+          })
+      }).then(challenge => {
+        const games = challenge.games
+        const starsPromises = games.map(game => {
+          return knex('star').where({ game_id: game.id })
+        })
+
+        return Promise.all(starsPromises).then(starsSets => {
+          starsSets.forEach((stars, i) => {
+            games[i].stars = stars
+          })
+
+          challenge.games = games
+          return challenge
+        })
+      })
   }
 
   static getGamesByChallenge(id){
@@ -17,7 +42,6 @@ class ChallengeModel {
 
   static getStarsByChallenge(id){
     return knex('challenge_game')
-      // .select('challenge_game.*')
       .innerJoin('star', 'star.game_id', 'challenge_game.game_id')
       .where('challenge_id', id)
   }
